caccbca3d999dbd4a12338a41c60b978aa817d19
WPbody <contacto@wpbody.com>
2016-04-24 03:32:04 -0500

    Update wpbody-loggin-errors.php

--- wpbody-loggin-errors.php
+++ wpbody-loggin-errors.php
@@ -1,12 +1,20 @@
 <?php
 
 add_filter( 'login_errors', 'login_error_message' );
+
 function login_error_message( $error ) {
+    /**
+     * Global variable $errors 
+     */
     global $errors;
+    /**
+     * Prepare the error to view to the admin
+     */
     $error = sprintf( ucwords( str_replace( '_', ' ', $errors->get_error_code() ) ), '%s' );
     if( $errors->get_error_code() ) {
         $error;
     }
+    // Return the error message
     return $error;
 }
 
